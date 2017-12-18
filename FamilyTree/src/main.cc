108e928bfd73199d4538b19a43fe268e925a7c8a
Nick Loomis <nloomis@gmail.com>
2017-12-18 00:22:48 -0800

    royals example expanded for depth coverage

--- FamilyTree/src/main.cc
+++ FamilyTree/src/main.cc
@@ -6,11 +6,15 @@
 
 int main() {
   // British royal family, from http://www.bbc.com/news/uk-23272491
-  family_tree::Person p_qe2("Queen Elizabeth II", nullptr, nullptr);
+  family_tree::Person p_george5("George V", nullptr, nullptr);
+  family_tree::Person p_mary("Mary", nullptr, nullptr);
+  family_tree::Person p_george6("George VI", &p_george5, &p_mary);
+  family_tree::Person p_elizabeth1("Elizabeth", nullptr, nullptr);
+  family_tree::Person p_qe2("Queen Elizabeth II", &p_george6, &p_elizabeth1);
   family_tree::Person p_philip("Phillip", nullptr, nullptr);
   family_tree::Person p_charles("Prince Charles", &p_qe2, &p_philip);
   family_tree::Person p_diana("Princess Diana", nullptr, nullptr);
-  family_tree::Person p_william("William", &p_diana, &p_charles);
+  family_tree::Person p_william("Prince William", &p_diana, &p_charles);
   family_tree::Person p_kate("Kate", nullptr, nullptr);
   family_tree::Person p_george("Prince George", &p_william, &p_kate);
   family_tree::Person p_charlotte("Princess Charlotte", &p_william, &p_kate);
@@ -20,19 +24,32 @@ int main() {
   family_tree::Person p_peter("Peter Phillips", &p_anne, &p_mark);
   family_tree::Person p_zara("Zara Tindall", &p_anne, &p_mark);
   family_tree::Person p_autumn("Autumn Phillips", nullptr, nullptr);
-  family_tree::Person p_savannah("Savannah", &p_peter, &p_autumn);
+  family_tree::Person p_savannah("Savannah Phillips", &p_peter, &p_autumn);
   family_tree::Person p_isla("Isla", &p_peter, &p_autumn);
   family_tree::Person p_mike("Mike Tindall", nullptr, nullptr);
   family_tree::Person p_mia("Mia Grace", &p_zara, &p_mike);
-  family_tree::Person p_andrew("Andrew", &p_qe2, &p_philip);
-  family_tree::Person p_sarah("Sarah", nullptr, nullptr);
+  family_tree::Person p_andrew("Prince Andrew", &p_qe2, &p_philip);
+  family_tree::Person p_sarah("Sarah Ferguson", nullptr, nullptr);
   family_tree::Person p_beatrice("Princess Beatrice", &p_andrew, &p_sarah);
   family_tree::Person p_eugenie("Princess Eugenie", &p_andrew, &p_sarah);
-  family_tree::Person p_edward("Edward", &p_qe2, &p_philip);
-  family_tree::Person p_sophie("Sophie", nullptr, nullptr);
+  family_tree::Person p_edward("Prince Edward", &p_qe2, &p_philip);
+  family_tree::Person p_sophie("Sophie Rhys-Jones", nullptr, nullptr);
   family_tree::Person p_louise("Louise", &p_edward, &p_sophie);
   family_tree::Person p_james("James", &p_edward, &p_sophie);
-
+  family_tree::Person p_margaret("Margaret", &p_george6, &p_elizabeth1);
+  family_tree::Person p_anthony("Anthony Armstrong-Jones", nullptr, nullptr);
+  family_tree::Person p_david("David Armstrong-Jones", &p_margaret, &p_anthony);
+  family_tree::Person p_edward8("Edward VIII", &p_george5, &p_mary);
+  family_tree::Person p_princessmary("Princess Mary", &p_george5, &p_mary);
+  family_tree::Person p_john("Prince John", &p_george5, &p_mary);
+  family_tree::Person p_henry("Prince Henry", &p_george5, &p_mary);
+  family_tree::Person p_georgekent("Prince George", &p_george5, &p_mary);
+  family_tree::Person p_alice("Alice Montegu", nullptr, nullptr);
+  family_tree::Person p_richard("Richard", &p_henry, &p_alice);
+  family_tree::Person p_marina("Princess Marina", nullptr, nullptr);
+  family_tree::Person p_edwardkent("Edward", &p_georgekent, &p_marina);
+  family_tree::Person p_alexandra("Princess Alexandra", &p_georgekent, &p_marina);
+  family_tree::Person p_michael("Prince Michael", &p_georgekent, &p_marina);
 
 /*	family_tree::Person person_adad("Alice's dad", nullptr, nullptr);
 	family_tree::Person person_amom("Alice's mom", nullptr, nullptr);
@@ -79,7 +96,9 @@ int main() {
   p_qe2.GetFamilyTree(&grapher, graph_file);
 
   std::ofstream harry_graph("harry_graph.gv");
-  p_harry.GetFamilyTree(&grapher, harry_graph);
+  options.ancestor_depth = 2;
+  family_tree::GraphEmitter harry_grapher(options);
+  p_harry.GetFamilyTree(&harry_grapher, harry_graph);
 
   // tree_builder.GraphString();
 
